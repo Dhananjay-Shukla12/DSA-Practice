1c8393a1d8657d88a39f484b59b80036047c2585
Dhananjay Shukla <dhananjayshukla@Dhananjays-MacBook-Air.local>
2025-06-09 17:40:59 +0530

    Codeforces: 1029B shrink

---
+++ Codeforces/1029BShrink.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi; 
+typedef pair<int,int> pi;
+#define PB push_back
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+ll t;
+cin>>t;
+while(t--){
+    ll n;
+    cin>>n;
+    cout<<2<<" ";
+    for(ll i=3;i<=n;++i)cout<<i<<" ";
+    cout<<1<<endl;
+}
+return 0;
+
+}
+
